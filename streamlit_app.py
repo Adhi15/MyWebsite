1d31df2baaf5a1f2501db25afa8ea3e128afd9d2
Adhi AU <50472175+Adhi15@users.noreply.github.com>
2023-01-23 15:53:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,16 +10,18 @@ with open("style.css") as f:
 # Header 
 st.write('''
 # Adhihariharan AU
-##### *Portfolio* 
 ''')
 
 image = Image.open('Profile.jpg')
 st.image(image, width=150)
 
+
 st.markdown('## Summary', unsafe_allow_html=True)
 st.info('''
-- An enthusiastic and high energy driven professional aiming for entry-level assignments in data science, data analytics with an organization of high repute offering challenging work profile in the it industry.
-- Strong verbal and written communication skills.
+- Hi, my name is Adhi. I am 22 years old. Currently pursuing my master at SMU,Singapore. 
+- I am an enthusiastic and high energy driven professional aiming to land a role in data science, data analytics or software development with an organization of high repute offering challenging work profile in the IT industry. Have strong verbal and written communication skills.
+- DOB: 15th January 2001
+- Gmail: adhi15au@gmail.com
 ''')
 
 #####################
@@ -98,20 +100,30 @@ st.markdown('''
 
 txt('**Masters of Science** (Copmuting, Data Science and Engineering Research track), *Singapore Management University*, Sinagpore',
 '2022-current')
-st.markdown('''
-- GPA: `2.57`/`4`
-''')
 
 txt('**Bachelors of Engeering** (Copmuter Science and Engineering), *Sri Venkateswara Coellege of Engineering*, India',
 '2018-2022')
 st.markdown('''
-- GPA: `8.38`/`10`
 - Graduated with First Class Honors.
 ''')
 
 #####################
 st.markdown('''
 ## Work Experience
+''')
+
+txt('**IEEE SSCI 2022**, Student Helper, Singapore Management University, Singapore',
+'2022')
+st.markdown('''
+- The IEEE SSCI 2022 conference was conducted at SMU Singapore. 
+- As the student helper I along with a few others helped in conducting the event for 4 days. 
+- In this event, I had the opportunity to talk and listen to various researchers and Professors was a very insightful experience.
+''')
+
+txt('**Youth Red Cross**, Member, India',
+'2020-2022')
+st.markdown('''
+- Was part of the youth red cross. Participated in various events conducted in our college. 
 ''')
 
 txt('**FORESE**','')
@@ -138,26 +150,6 @@ st.markdown('''
 - As the Vice President as the club. I had to oversee the juniors work in conducting the event. 
 ''')
 
-txt('**IEEE SSCI 2022**, Student Helper, Singapore Management University, Singapore',
-'2022')
-st.markdown('''
-- The IEEE SSCI 2022 conference was conducted at SMU Singapore. 
-- As the student helper I along with a few others helped in conducting the event for 4 days. 
-- In this event, I had the opportunity to talk and listen to various researchers and Professors was a very insightful experience.
-''')
-
-txt('**Glosys Technology Solutions Pvt. Ltd.**, Trainee, India',
-'2019')
-st.markdown('''
-- I was a Data science trainee in this company. I finished a four month course in the Data science using Python course. 
-''')
-
-txt('**Youth Red Cross**, Member, India',
-'2020-2022')
-st.markdown('''
-- Was part of the youth red cross. Participated in various events conducted in our college. 
-''')
-
 txt('**The Sparks Foundation**, Data Science and Business analytics Intern, Singapore',
 '2021')
 st.markdown('''
@@ -166,13 +158,18 @@ st.markdown('''
 - I was also tasked to update my Linkedin and also the company helped me increase my connections on Linkedin. 
 ''')
 
+txt('**Glosys Technology Solutions Pvt. Ltd.**, Trainee, India',
+'2019')
+st.markdown('''
+- I was a Data science trainee in this company. I finished a four month course in the Data science using Python course. 
+''')
 
 
 #####################
 st.markdown('''
 ## Projects
 ''')
-txt('**DETECTION OF MALICIOUSLY TAMPERED MEDICAL IMAGES USING ANOMOLY DETECTION**', '')
+# txt('**DETECTION OF MALICIOUSLY TAMPERED MEDICAL IMAGES USING ANOMOLY DETECTION**', '')
 txt('**SMART TRAFFIC LIGHT DETECTION**', '')
 st.markdown('''
 https://github.com/Adhi15/Smart-Traffic-Light-Control-System/tree/main 
@@ -204,11 +201,71 @@ And i had taken that course. As part of the course I was tasked to do a capstone
 The problem statement given to me was, Bellabeat a fictional company has tasked me to focus on one of Bellabeat products and analyze smart device data to gain insight into how consumers are using their smart devices. 
 This is my capstone project for the Google Data Analytics Professional Certificate. As a novice who was learning about data visualisation and data cleaning it was a challenge task and an interesting project to spend my time in. 
 ''')
-txt('**SYN-FLOOD ATTACK SIMULATION AND DETECTION**', '')
+
 txt('**ANALYZING NFT INDEXING CAUSED BY A LARGE-SCALE SOCIAL MEDIA PLATFORM**', '')
+st.markdown('''
+The basic objective of this Project:
+- Figure out the relationship between social media platforms and NFTs on OpenSea.
+- Create a value prediction model for NFTs based on information gathered from social media and OpenSea.
+- We collect information announcing NFTs and follow the linked OpenSea URLs to extract NFT sales information and metadata from OpenSea.
+- We crawl NFT images to analyse whether the image in itself exerts an influence on the price. Analysing growth and price trends on OpenSea against popularity metrics from Social Media platform reveals the possible significance of social media features on NFT value. We motivate average selling price as the metric to assign value to an NFT due to limited sales and highly volatile prices of NFT. We further develop and analyse predictive models using Social Media platform, OpenSea and NFT image features to assess their impact on asset value. Basically, 
+- To the best of our knowledge, we create a data set for NFTs from OpenSea and their corresponding Social Media Input (can be a tweet, subreddit announcement, discord server announcement).
+- We build ordinal classification models to predict NFT asset value using features from both OpenSea and Social Media, as well as the NFT image itself.
+- We show that both Social Media Platform and OpenSea features influence the model output. In contrast the predictive power of image features is limited. This shows how branding and metadata (Social Media and OpenSea features) have a stronger effect on the value compared to the NFT product itself.
+- We discuss related work on NFTs and asset valuation in the financial domain. 
+- We briefly introduce the OpenSea platform and its features, as well as blockchain-specific terms used in our analysis.
+- We discuss our data collection pipeline and analyse the impact of Social Media on the OpenSea marketplace from the temporal dimension and value perspective. 
+- We discuss multimodal models for NFT asset valuation, present results using various models and feature subsets. At last we conclude with the limitations.
+This was my final year project for my bachelor's degree.
+''')
+txt('**SYN-FLOOD ATTACK SIMULATION AND DETECTION**', '')
+
+st.markdown('''
+- The method in which we want to implement our work is by using a Kali Linux virtual machine to attack an Ubuntu virtual machine. 
+- The Kali Linux machine uses the hping3 tool which is pre-installed inside the Kali Linux is used to flood the packets into the victim Ubuntu Machine. 
+- We also perform IP spoofing which is a part of the tool. 
+- The live capturing of packets using the Wireshark tool collects the data from the victim machine while the attack happens and stores it. 
+- We use an open-source dataset, clean it and process it.
+- Using Python packages and machine learning modules such as SVM, KNN, Gaussian NB, Random Forest, analyzation tree to classify the various DDoS attacks.
+- In this project,we explore the various scopes of DDoS and DoS attacks and attempt to detect a DoS Attack with the help of Artificial Intelligence.
+- We also give a glimpse of how DoS is executed and analyze DoS attack datasets to present a vivid visualization. 
+- Our primary intention for this work is to create awareness about DDoS attacks to the general public and also to allure people's attention towards the importance of Cyber Security in the modern world.
+This project was done as a group mini project during my bachelors degree.
+''')
+
 txt('**PREDICTING IMAGE SEQUENCING**', '')
+st.markdown('''
+In this work, 
+- We are presented with the task of predicting image sequencing. 
+- Image sequencing is a challenging task, but it has significant practical importance as well. 
+- In this work, we focus on predicting the unknown order of three images given the order of the other five images of the same sequence. 
+- Intuitively, we transfer the image sequencing task into a special video frame prediction problem and propose a model that relies on ResNet18 and ConvLSTM to help predict the order. 
+- We conduct experiments on the given dataset and demonstrate that the performance of our model outperforms other baselines.
+This project was done as a group project in one of the modules i took at SMU.
+''')
+
 txt('**SEMANTIC SEGMENTATION IN A WEAKLY SUPERVISED SETTING WITH ONLY IMAGE-LEVEL LABELS**', '')
+st.markdown('''
+For this project, 
+- We address the problem of semantic seg- mentation in a weakly supervised setting with only image- level labels. 
+- We provide a method for semantic segmenta- tion which is trained and evaluated on a given data set with 20 class labels. 
+- Class activation maps (CAMs) are com- monly used to identify semantic segmentation problems. Us- ing the popular PuzzleCAM we generate labels on a pixel level. 
+- Finally, we use the labels to generate the pseudo mask using inference segmentation. 
+- We show the performance of our approach in both public and private leaderboards.
+This project was done as a group project in one of the modules i took at SMU.
+''')
+
 txt('**FAKE NEWS DETECTION**', '')
+st.markdown('''
+Using human manual detection to detect fake news is not feasible as millions of articles around the world are being published and removed every minute and even seconds. 
+News detection cannot be accountable or feasible manually. 
+
+- To resolve the fake news issue is to create a model that will detect if the article is real or fake news using the  words, phrases, sources and titles from the article. 
+- In order to do so, first apply a supervised learning algorithm on a labelled dataset, and train the model using training data followed by testing data before using it on real data. Before performing on real data, first perform a feature selection.
+- As a result, we will be creating the model using different classification algorithms to choose the best fit classifier to obtain the highest F1 Score based on the calculated confusion matrix results.
+- We also create a telegram bot that will help in fake news detection.
+This project was done as a group project in one of the modules i took at SMU.
+''')
 
 
 #####################
@@ -229,3 +286,4 @@ st.markdown('''
 ''')
 txt2('LinkedIn', 'https://www.linkedin.com/in/adhihariharan-au-34b167171/')
 txt2('GitHub', 'https://github.com/Adhi15')
+
